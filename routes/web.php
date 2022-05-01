d7779c87a49037d0198004eb117bfa7eca12b94f
Brad Singleton <bsingleton.uk@gmail.com>
2022-05-01 20:16:46 +0100

    Add dummy views and update navigation and create routes for Books, Wishlist, and Bookshelf

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,15 @@ Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', fn () => view('dashboard'))->name('dashboard');
+
+    Route::get('/books', fn () => view('books.index'))->name('books.index');
+
+    Route::get('/wishlist', fn () => view('wishlist.index'))->name('wishlist.index');
+
+    Route::get('/bookshelf', fn () => view('bookshelf.index'))->name('bookshelf.index');
+
+});
+
 require __DIR__.'/auth.php';
