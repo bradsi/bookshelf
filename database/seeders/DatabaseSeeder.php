adcc05e9cb540f4625cd87d2b3ec01ddf08d5a94
Brad Singleton <bsingleton.uk@gmail.com>
2022-05-01 20:15:03 +0100

    Create author_book pivot table, create user factory, update database seeder to call Publisher Author and Book seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            PublisherSeeder::class,
+            AuthorSeeder::class,
+            BookSeeder::class,
+        ]);
+
+        for ($i = 0; $i <= rand(100, 400); $i++) {
+            DB::table('author_book')->insert([
+                'author_id' => rand(1, 4000),
+                'book_id' => rand(1, 5000)
+            ]);
+        }
     }
 }
